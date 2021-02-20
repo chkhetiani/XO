47e116c5a0259e022809b215106678c0040b6319
SabaGvaramadze <79360799+SabaGvaramadze@users.noreply.github.com>
2021-02-20 08:32:21 -0800

    Update drawer/movemaker

--- lib/drawer.h
+++ lib/drawer.h
@@ -1,6 +1,10 @@
 namespace xo {
 	void draw() {
-		// board
-		// board_length
+		for(int i=0;i<board_length-'0';i++){
+			cout << board[i];
+			if(i==3||i==6){
+				cout << "\n";
+			}
+		}
 	}
 }
--- lib/movemaker.h
+++ lib/movemaker.h
@@ -1,10 +1,17 @@
 namespace xo {
 	void make_move(int index) {
+		board[index] = to_move;
 		// to_move
 		// board
 	}
 	
 	void set_player() {
+		if (to_move == 'O'){
+			to_move = 'X';
+		}
+		else {
+			to_move = 'O';
+		}
 		// to_move
 	}
 }
