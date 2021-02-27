2aa864723c034a74a5fb4a2d201254769b3cc570
Irakli Chkhetiaini <chkhetiani.i@gmail.com>
2021-02-27 12:35:21 +0400

    drawer

--- lib/drawer.h
+++ lib/drawer.h
@@ -1,10 +1,9 @@
 namespace xo {
 	void draw() {
 		// board
-		// board_length
-		
-		std::cout << board[0] << " " << board[1] << " " << board[2] << std::endl;
-		std::cout << board[2] << " " << board[3] << " " << board[4] << std::endl;
-		std::cout << "dafa4";
+		// board_length	
+		std::cout << board[0] << "|" << board[1] << "|" << board[2] << std::endl;
+		std::cout << board[3] << "|" << board[4] << "|" << board[5] << std::endl;
+		std::cout << board[6] << "|" << board[7] << "|" << board[8] << std::endl;
 	}
 }
