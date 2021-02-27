13a506ef009c04366d6205aadbeec825a25fea0d
sopcommand <75880117+sopcommand@users.noreply.github.com>
2021-02-27 14:02:12 +0400

    FullDraw
    DRAWERRRR

--- lib/drawer.h
+++ lib/drawer.h
@@ -2,7 +2,9 @@ namespace xo {
 	void draw() {
 		// board
 		// board_length
-		std::cout<<board[0]<<"_|"<<board[1]<<"_|"<<board[3]<<"_|"<<std::endl;
+		std::cout<<"| "<<board[0]<< " | " <<board[1]<< " | " <<board[2]<< " | " <<std::endl;
+		std::cout<<"| "<<board[3]<< " | " <<board[4]<< " | " <<board[5]<< " | " <<std::endl;
+		std::cout<<"| "<<board[6]<< " | " <<board[7]<< " | " <<board[8]<< " | " <<std::endl;
 		
 	}
 }
