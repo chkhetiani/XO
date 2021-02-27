c351ddc2dd063ebe9dbba3f671388e8398b075f4
Irakli Chkhetiaini <chkhetiani.i@gmail.com>
2021-02-27 14:06:45 +0400

    update data.h

--- lib/data.h
+++ lib/data.h
@@ -1,7 +1,7 @@
 #include<string>
 
 namespace xo {
-	char board_length = 9;
+	int board_length = 9;
 	char board[] = { '-','-','-','-','-','-','-','-','-' };
 	// ----------------------
 	char to_move = 'O';
