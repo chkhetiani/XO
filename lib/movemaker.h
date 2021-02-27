dd83246da907cd16d070e6b79c88b52d789afba2
cicruitmaster10 <sabachumburidze247@gmail.com>
2021-02-27 13:41:09 +0400

    Update movemaker.h
    movemaker

--- lib/movemaker.h
+++ lib/movemaker.h
@@ -2,9 +2,20 @@ namespace xo {
 	void make_move(int index) {
 		// to_move
 		// board
+		
+		board[index]=to_move;
 	}
 	
 	void set_player() {
 		// to_move
+		if(to_move == 'o'){
+		
+			to_move='x';
+	
+		}
+	
+		else{
+			to_move='o'
+		}
 	}
 }
