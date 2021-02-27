56f81fdda3a2c019635837f528a0faace1435100
kakauridze1 <79003585+kakauridze1@users.noreply.github.com>
2021-02-27 12:38:01 +0400

    bord

--- lib/drawer.h
+++ lib/drawer.h
@@ -1,6 +1,7 @@
 namespace xo {
 	void draw() {
-		// board
-		// board_length
-	}
+		std::cout <<  board[0] << " | "<<  board[1] << " | " <<  board[2] << " | "<< std :: endl;
+		std::cout <<  board[3] << " | " << board[4] << " | " <<  board[5] << " | "<< std:: edndl;
+		
+			}
 }
