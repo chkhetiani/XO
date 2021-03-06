80b54554e02b0b9e34f71c7a121202ff526a7672
Khalatashvili <dimitri.khalatashvili20@gmail.com>
2021-03-06 12:21:34 +0400

    update movemaker

--- lib/movemaker.h
+++ lib/movemaker.h
@@ -2,9 +2,16 @@ namespace xo {
 	void make_move(int index) {
 		// to_move
 		// board
+		board[index]= to_move;
 	}
 	
 	void set_player() {
+		if(to_move == 'X'){
+			to_move = 'O'
+		}
+		else{
+			to_move = 'X'
+		}
 		// to_move
 	}
 }
