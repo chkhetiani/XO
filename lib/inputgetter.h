982ff9542b1d63f01aefcfc5fcab52216e727807
Khalatashvili <dimitri.khalatashvili20@gmail.com>
2021-03-06 11:02:44 +0400

    input update

--- lib/inputgetter.h
+++ lib/inputgetter.h
@@ -1,5 +1,14 @@
 namespace xo {
 	int get_input() {
 		// get input and return
+		
+		int index;
+		
+		std::cout<< "Choose ur index to make move: ";
+		std::cin>> index;
+		
+		return index; 
+		
+	
 	}
 }
