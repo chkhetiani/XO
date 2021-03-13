eed76a157e6d4554210154aef03c02399f2d9413
69Lucifer69 <77882876+69Lucifer69@users.noreply.github.com>
2021-03-13 12:40:57 +0400

    ...

--- lib/inputgetter.h
+++ lib/inputgetter.h
@@ -2,12 +2,16 @@ namespace xo {
 	int get_input() {
 				 int to_move;
 
-    if (turn == 'X') {
-        std::cout << "\tPLAYER 1[X] TURN :";
-        std::cin >> to_move;
-    }
-    if (turn == 'O') {
-        std::cout << "\tPLAYER 2[O] TURN :";
-        std::cin >> to_move;
-    }
-	}
+					int choise;
+	    if (to_move == 'X') {
+		        std::cout << "\tPLAYER 1[X] TURN :";
+		        std::cin >> choise;
+    	}
+	    if (to_move == 'O') {
+		        std::cout << "\tPLAYER 2[O] TURN :";
+		        std::cin >> choise;
+    	}
+	}//Get Input(Is Figuruli Frchxili) 2 XAZZE'
+	
+}
+
--- lib/winchecker.h
+++ lib/winchecker.h
@@ -5,76 +5,77 @@ namespace xo {
 		// has_finished = true;
 		// result = "X won" || "O won" || "draw"
 
-		if(board[0] == 'X' && board[1] == 'X' && board[2] == 'X' ){
-    has_finished = true; 
-    result = "X Won!";
-}
+			if(board[0] == 'X' && board[1] == 'X' && board[2] == 'X' ){
+			    has_finished = true; 
+			    result = "X Won!";
+		}
 
 
-		if(board[0] == 'O' && board[1] == 'O' && board[2] == 'O' ){
-    has_finished = true; 
-    result = "O Won!";
-}  
+			if(board[0] == 'O' && board[1] == 'O' && board[2] == 'O' ){
+			    has_finished = true; 
+			    result = "O Won!";
+		}  
 
 
-        if(board[3] == 'X' && board[4] == 'X' && board[5] == 'X'){
-    has_finished = true;
-    result = "X Won!";
-}
+	        if(board[3] == 'X' && board[4] == 'X' && board[5] == 'X'){
+			    has_finished = true;
+			    result = "X Won!";
+		}
 
 
-        if(board[3] == 'O' && board[4] == 'O' && board[5] == 'O'){
-    has_finished = true;
-    result = "O Won!";
-}
+	        if(board[3] == 'O' && board[4] == 'O' && board[5] == 'O'){
+			    has_finished = true;
+			    result = "O Won!";
+		}
 
 
-        if(board[6] == 'X' && board[7] == 'X' && board[8] == 'X'){
-    has_finished = true;
-    result = "X Won!";
-}
+	        if(board[6] == 'X' && board[7] == 'X' && board[8] == 'X'){
+			    has_finished = true;
+			    result = "X Won!";
+		}
 
 
-        if(board[6] == 'O' && board[7] == 'O' && board[8] == 'O'){
-    has_finished = true;
-    result = "O Won!";
-}
+	        if(board[6] == 'O' && board[7] == 'O' && board[8] == 'O'){
+			    has_finished = true;
+			    result = "O Won!";
+		}
 
 
-        if(board[0] == 'X' && board[3] == 'X' && board[6] == 'X'){
-    has_finished = true;
-    result = "X Won!";
-}
+	        if(board[0] == 'X' && board[3] == 'X' && board[6] == 'X'){
+			    has_finished = true;
+			    result = "X Won!";
+		}
 
 
-        if(board[0] == 'O' && board[3] == 'O' && board[6] == 'O'){
-    has_finished = true;
-    result = "O Won!";
-}
+	        if(board[0] == 'O' && board[3] == 'O' && board[6] == 'O'){
+			    has_finished = true;
+			    result = "O Won!";
+		}
 
 
-        if(board[1] == 'X' && board[4] == 'X' && board[7] == 'X'){
-    has_finished = true;
-    result = "X Won!";
-}
+	        if(board[1] == 'X' && board[4] == 'X' && board[7] == 'X'){
+			    has_finished = true;
+			    result = "X Won!";
+		}
 
 
-        if(board[1] == 'O' && board[4] == 'X' && board[7] == 'O'){
-    has_finished = true;
-    result = "O Won!";
-}
+	        if(board[1] == 'O' && board[4] == 'O' && board[7] == 'O'){
+			    has_finished = true;
+			    result = "O Won!";
+		}
 
 
-        if(board[2] == 'X' && board[5] == 'X' && board[8] == 'X'){
-    has_finished = true;
-    result = "X Won!";
-}
+	        if(board[2] == 'X' && board[5] == 'X' && board[8] == 'X'){
+			    has_finished = true;
+			    result = "X Won!";
+		}
 
 
-        if(board[2] == 'O' && board[5] == 'O' && board[8] == 'O'){
-    has_finished = true;
-    result = "O Won!";
-}
+	        if(board[2] == 'O' && board[5] == 'O' && board[8] == 'O'){
+			    has_finished = true;
+			    result = "O Won!";
+		}
 
+	}//Bool Check_Win(Is Figuruli Frchxili)
 
 }
