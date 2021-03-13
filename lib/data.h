65840acdb93bbd3aed9e9b409e1f48a8daa95bac
69Lucifer69 <77882876+69Lucifer69@users.noreply.github.com>
2021-03-13 13:13:34 +0400

    TAVISKDEBA ZMAO

--- lib/data.h
+++ lib/data.h
@@ -2,7 +2,7 @@
 
 namespace xo {
 	char board_length = 9;
-	char board[] = { '-','-','-','-','-','-','-','-','-' };
+	char board[] = { '0','1','2','3','4','5','6','7','8' };
 	// ----------------------
 	char to_move = 'O';
 	bool has_finished = false;
