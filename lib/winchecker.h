a7023ac999120195701ca83160dc5d8643751dd0
69Lucifer69 <77882876+69Lucifer69@users.noreply.github.com>
2021-03-13 11:53:01 +0400

    Mhm Shevaswore

--- lib/winchecker.h
+++ lib/winchecker.h
@@ -19,61 +19,61 @@ namespace xo {
 
         if(board[3] == 'X' && board[4] == 'X' && board[5] == 'X'){
     has_finished = true;
-    result = "X Won!"
+    result = "X Won!";
 }
 
 
         if(board[3] == 'O' && board[4] == 'O' && board[5] == 'O'){
     has_finished = true;
-    result = "O Won!"
+    result = "O Won!";
 }
 
 
         if(board[6] == 'X' && board[7] == 'X' && board[8] == 'X'){
     has_finished = true;
-    results = "X Won!"
+    result = "X Won!";
 }
 
 
         if(board[6] == 'O' && board[7] == 'O' && board[8] == 'O'){
     has_finished = true;
-    results = "O Won!"
+    result = "O Won!";
 }
 
 
         if(board[0] == 'X' && board[3] == 'X' && board[6] == 'X'){
     has_finished = true;
-    results = "X Won!"
+    result = "X Won!";
 }
 
 
         if(board[0] == 'O' && board[3] == 'O' && board[6] == 'O'){
     has_finished = true;
-    results = "O Won!"
+    result = "O Won!";
 }
 
 
         if(board[1] == 'X' && board[4] == 'X' && board[7] == 'X'){
     has_finished = true;
-    results = "X Won!"
+    result = "X Won!";
 }
 
 
         if(board[1] == 'O' && board[4] == 'X' && board[7] == 'O'){
     has_finished = true;
-    results = "O Won!"
+    result = "O Won!";
 }
 
 
         if(board[2] == 'X' && board[5] == 'X' && board[8] == 'X'){
     has_finished = true;
-    results = "X Won!"
+    result = "X Won!";
 }
 
 
         if(board[2] == 'O' && board[5] == 'O' && board[8] == 'O'){
     has_finished = true;
-    results = "O Won!"
+    result = "O Won!";
 }
 
 
